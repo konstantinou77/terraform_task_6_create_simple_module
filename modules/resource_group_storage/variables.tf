6bea5ca8a5e5c131bd373ae4dc2791118ae684e7
konstantinou77 <kostiakryvulia@gmail.com>
2025-05-20 20:24:37 +0300

    fix fmt

--- modules/resource_group_storage/variables.tf
+++ modules/resource_group_storage/variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  default     =   "example-resources"
+  default     = "example-resources"
 }
 
 variable "location" {
   description = "The Azure location for the resource group"
-  default     =   "East US"
+  default     = "East US"
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
-  default     =   "examplestorageaccount"
+  default     = "examplestorageaccount"
 }
